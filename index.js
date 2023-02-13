66609a8f8f17b92bb540c05a909e61d7f80b10b3
jiangjili <jili.jiang@palusainc.com>
2023-02-13 11:58:03 -0800

    fixed add employees to pass two separate query results

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const enquirer = require('enquirer')
 const { prompt, Select } = require('enquirer')
 const db = require("./db/connection.js");
 
@@ -34,7 +35,7 @@ const mainMenu = async () => {
       case "View All Employees": return
       case "View All Employees By Department": return
       case "View All Employees By Manager": return
-      case "Add Employee": return
+      case "Add Employee": return addEmployee()
       case "Remove Employee": return
       case "Update Employee Role": return
       case "Update Employee Manager": return
@@ -78,5 +79,67 @@ async function addEmployeeRole() {
    db.query(`SELECT * FROM role`, (err, res) => console.table(res));
    mainMenu()
 }
+
+
+function addEmployee() {
+
+   let firstList = [];
+   let secondList = [];
+
+   function handleLists(err) {
+      if (err) throw err;
+
+      // Check if both lists have been retrieved
+      if (firstList.length > 0 && secondList.length > 0) {
+         // Pass the lists as choices to an inquirer prompt
+         const prompt1 = new enquirer.Select(
+            {
+               name: "choice",
+               type: "list",
+               message: "Select a role:",
+               choices: [...firstList]
+            },
+         );
+
+         prompt1.run()
+            .then(answer1 => {
+               console.log("role:", answer1)
+               const prompt2 = new enquirer.Select(
+                  {
+                     name: "choice",
+                     type: "list",
+                     message: "Select a manager:",
+                     choices: [...secondList]
+                  },
+               );
+
+               prompt2.run()
+                  .then(answer2 => {
+                     console.log("manager:", answer2);
+                     console.table([answer1, answer2])
+                  })
+                  .catch(console.error);
+            }
+            )
+            .catch(console.error);
+      }
+   }
+
+
+   db.query("SELECT * FROM role", (err, results) => {
+      if (err) throw err;
+      // Map the results to get only the relevant data
+      firstList = results.map(result => result.title);
+      handleLists(err);
+   });
+
+   db.query("SELECT * FROM employee", (err, results) => {
+      if (err) throw err;
+      // Map the results to get only the relevant data
+      secondList = results.map(result => result.last_name);
+      handleLists(err);
+   });
+};
+
 
 mainMenu();
